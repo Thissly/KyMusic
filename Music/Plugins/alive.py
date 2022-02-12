051ca25cafde976901f6609655ecf79b84b165a9
Thiss <90003628+Thissly@users.noreply.github.com>
2022-02-12 17:23:07 +0700

    Update alive.py

--- Music/Plugins/alive.py
+++ Music/Plugins/alive.py
@@ -42,7 +42,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-BOT_IMG = "https://telegra.ph/file/2b610c918dec590d2777b.jpg"
+BOT_IMG = "https://telegra.ph/file/7ede265010503c2ec756f.jpg"
 
 
 @app.on_message(filters.command(["alive", "alive@Tg_Vc_00_Bot"]))
