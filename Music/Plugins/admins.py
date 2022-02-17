6c9052ed6fa8bc342952e7977a312a511e38bb3e
Thiss <90003628+Thissly@users.noreply.github.com>
2022-02-17 22:47:57 +0700

    Update admins.py

--- Music/Plugins/admins.py
+++ Music/Plugins/admins.py
@@ -310,5 +310,5 @@ async def reload(_, message):
     chat_id = message.chat.id
     await _.send_message(
     chat_id,
-    "✅ Bot dimulai ulang **berhasil**\n\n✅ **Admin** daftar telah **diperbarui**"
+    "✅ Bot berhasil di mulai ulang\n\n✅ Daftar admin telah di perbarui"
 )
